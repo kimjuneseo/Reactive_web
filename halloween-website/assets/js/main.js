95704186bf3892cf4f5f495b2a08c4190cfcaff2
kimjuneseo <june3929213512@gmail.com>
2021-11-08 18:06:42 +0900

    시작'

---
+++ halloween-website/assets/js/main.js
@@ -0,0 +1,30 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== HOME SWIPER ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
