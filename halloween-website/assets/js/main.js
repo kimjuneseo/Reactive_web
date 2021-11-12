4ebc5ea8f938be1492f1ff234de47583c6ec6a14
kimjuneseo <june3929213512@gmail.com>
2021-11-12 11:39:07 +0900

    모바일

--- halloween-website/assets/js/main.js
+++ halloween-website/assets/js/main.js
@@ -1,25 +1,63 @@
-/*=============== SHOW MENU ===============*/
+const nav = document.querySelector('.nav');
+const showMenu = document.querySelector('.show_menu');
+const hiddenMenu = document.querySelector('.hidden_menu');
 
+/*=============== SHOW MENU ===============*/
+window.addEventListener('scroll', (e) => {
+    console.log(e);
+})
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
-
+showMenu.addEventListener('click', () => {
+    nav.style.height = '400px'
+})
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+hiddenMenu.addEventListener('click', () => {
+    nav.style.height = 0;
+})
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
 /*=============== HOME SWIPER ===============*/
-
+const swiperHome = new Swiper('.swiper-home', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        clickable: true,
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
 /*=============== NEW SWIPER ===============*/
-
+const swiper = new Swiper('.swiper-arrivals', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination1',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        clickable: true,
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
